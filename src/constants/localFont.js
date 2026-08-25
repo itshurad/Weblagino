68c0e1bc4412d6736b9d310cd43e355912e03614
Hurad <maliravi.dev@gmail.com>
2026-08-25 17:23:10 +0330

    .

--- src/constants/localFont.js
+++ src/constants/localFont.js
@@ -3,57 +3,57 @@ import LocalFont from "next/font/local";
 const iranFont = LocalFont({
   src: [
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Thin.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Thin.woff2",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-UltraLight.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-UltraLight.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Light.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Regular.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Medium.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-DemiBold.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-DemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Bold.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-ExtraBold.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Black.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Black.woff2",
       weight: "900",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-ExtraBlack.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-ExtraBlack.woff2",
       weight: "950",
       style: "normal",
     },
     {
-      path: "../../public/fonts/IranYekan/IranYekanXFaNum-Heavy.woff2",
+      path: "../../public/fonts/IranYekan/IRANYekanXFaNum-Heavy.woff2",
       weight: "1000",
       style: "normal",
     },
